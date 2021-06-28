7b52bfe443aed39cb3c1db965e823b27db6c8a22
saman aslani <saman.aslani1992@yahoo.com>
2021-06-28 12:09:12 +0430

    Add files via upload

---
+++ demoRabbit/src/main/java/com/example/demorabbit/DemoRabbitApplication.java
@@ -0,0 +1,12 @@
+package com.example.demorabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoRabbitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRabbitApplication.class, args);
+    }
+}
